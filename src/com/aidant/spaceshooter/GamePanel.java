08bf7184e9df12d585777b078cda149c5d34cf86
AidanTilanus <Aidan.tilanus@gmail.com>
2024-10-01 19:58:43 +0200

    made a basic game loop

---
+++ src/com/aidant/spaceshooter/GamePanel.java
@@ -0,0 +1,74 @@
+package com.aidant.spaceshooter;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    // SCREEN SETTINGS
+    final int scale = 3;
+
+    final int screenWidth = 128 * scale;
+    final int screenHeight = 256 * scale;
+
+    // FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        double drawInterval = 1_000_000_000 / FPS; // 0.01666.. seconds
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while(gameThread != null) {
+
+            update();
+
+            repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime /= 1_000_000;
+
+                if(remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+    }
+}
